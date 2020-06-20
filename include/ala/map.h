dbb68b6de096f1e5ff3de8d3839ef6d946aae14d
ala <moeala@outlook.com>
2020-06-20 10:41:09 +0800

    Fix inc file name

--- include/ala/map.h
+++ include/ala/map.h
@@ -2,12 +2,12 @@
 #define _ALA_MAP_H
 
 #define _ALA_IS_MAP 1
-#include <ala/detail/tree_node_inc.h>
+#include <ala/detail/tree_node.inc>
 #define _ALA_IS_UNIQ 1
-#include <ala/detail/tree_container_inc.h>
+#include <ala/detail/tree_container.inc>
 #undef _ALA_IS_UNIQ
 #define _ALA_IS_UNIQ 0
-#include <ala/detail/tree_container_inc.h>
+#include <ala/detail/tree_container.inc>
 
 #undef _ALA_IS_MAP
 #undef _ALA_IS_UNIQ
--- include/ala/set.h
+++ include/ala/set.h
@@ -2,12 +2,12 @@
 #define _ALA_SET_H
 
 #define _ALA_IS_MAP 0
-#include <ala/detail/tree_node_inc.h>
+#include <ala/detail/tree_node.inc>
 #define _ALA_IS_UNIQ 1
-#include <ala/detail/tree_container_inc.h>
+#include <ala/detail/tree_container.inc>
 #undef _ALA_IS_UNIQ
 #define _ALA_IS_UNIQ 0
-#include <ala/detail/tree_container_inc.h>
+#include <ala/detail/tree_container.inc>
 
 #undef _ALA_IS_MAP
 #undef _ALA_IS_UNIQ
